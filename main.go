52c96283b054581d5fca40d7022d823dbe72dc79
Aman <48248836+aman00323@users.noreply.github.com>
2022-04-26 19:45:53 +0530

    feat: remove testing method

--- main.go
+++ main.go
@@ -1,35 +1,10 @@
 package main
 
 import (
-	"context"
-	"fmt"
-
-	api "github.com/Improwised/coinmetrics-go-sdk/api/v4"
 	"github.com/Improwised/coinmetrics-go-sdk/coinmetrics"
 )
 
 // Init is used to initialize client object with the given endpoint and apikey
 func Init(endpoint, apikey string) (coinmetrics.CoinMetrics, error) {
 	return coinmetrics.InitClient(endpoint, apikey)
-}
-
-func main() {
-	c, err := Init(`https://api.coinmetrics.io/`, ``)
-	if err != nil {
-		panic(err)
-	}
-	mk := api.GetTimeseriesMarketGreeksParams{
-		Markets: api.MarketId(`deribit-ETH-25MAR22-1200-P-option`),
-	}
-	c.Limit(110)
-	res, errr := c.GetTimeseriesMarketGreeksWithResponseAsync(context.Background(), &mk)
-	for {
-		select {
-		case rr := <-res:
-			fmt.Println(`---------data-------`, rr)
-		case errc := <-errr:
-			fmt.Println("error", errc)
-			return
-		}
-	}
 }
